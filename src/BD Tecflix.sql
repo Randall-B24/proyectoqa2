6b3d234828a511282f8465998d38dcc4a687c652
psotoa <pedroa.sotoa@gmail.com>
2022-08-23 17:02:23 -0600

    Update BD Tecflix.sql

--- src/BD Tecflix.sql
+++ src/BD Tecflix.sql
@@ -5,7 +5,7 @@ create table Multimedia (
 id_video varchar(12) primary key,
 link varchar(300),
 titulo varchar(32) not null,
-emisor varchar(32) not null,
+emisor varchar(32) not null
 );
 
 create table ListaReproduccion (
@@ -21,6 +21,7 @@ nombre varchar(32)
 create table Lista_Video(
 id_video varchar(12),
 id_lista int,
+posicion_video int auto_increment,
 foreign key (id_video) references Multimedia(id_video)
 on delete cascade
 on update cascade,
@@ -30,7 +31,7 @@ on update cascade
 );
 
 create table Cola_Video(
-id_video int,
+id_video varchar(12),
 id_cola int,
 foreign key (id_video) references Multimedia(id_video)
 on delete cascade
@@ -40,16 +41,19 @@ on delete cascade
 on update cascade
 );
 
-insert into Multimedia values(1,'https://www.youtube.com/watch?v=eIjbSH3Imb8','Prueba','Pedro' ,'10:30');
-insert into Multimedia values(2,'https://www.youtube.com/watch?v=jqBuXibX9CM','Prueba','Pedro' ,'10:30');
+insert into Multimedia values('eIjbSH3Imb8','https://www.youtube.com/watch?v=eIjbSH3Imb8','Prueba','Pablo');
+insert into Multimedia values('jqBuXibX9CM','https://www.youtube.com/watch?v=jqBuXibX9CM','Prueba','Randall');
 
 select * from Multimedia;
 
 
-use tecflix;
-insert into listaReproduccion values(1,'lista1');
-insert into listaReproduccion values(2,'lista2');
+insert into ListaReproduccion values (1, 'prueba1');
+insert into ListaReproduccion values (2, 'prueba2');
 
+insert into Lista_Video values ('jqBuXibX9CM', 1);
+insert into Lista_Video values ('eIjbSH3Imb8', 2);
 
+select * from Lista_Video;
 
+Select LV.id_video, titulo from Lista_Video as LV JOIN ListaReproduccion as LR on LV.id_lista = LR.id_lista JOIN Multimedia as M on M.id_video = LV.id_video WHERE nombre = 'prueba2';
 
